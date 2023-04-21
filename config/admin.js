ca321d631ebf8984157a5968e7ee7d844e96a837
mrleedr <lgdelrosario@outlook.com>
2023-04-22 05:16:45 +0800

    update

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
+    apiToken: {
+      salt: env('API_TOKEN_SALT'),
+    },
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
 });
